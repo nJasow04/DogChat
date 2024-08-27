596bcf0f84887b4482d6f43e00118df661ea90b4
Jason Wan <njasow@g.ucla.edu>
2024-08-26 23:10:27 -0700

    Initial Commit

---
+++ DogChat/DogChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogChatApp.swift
+//  DogChat
+//
+//  Created by jason wan on 2024-08-26.
+//
+
+import SwiftUI
+
+@main
+struct DogChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
